de7367d120ac6a90150c8d9378f55119f8308ee2
Matheus Duarte <duarte.theus@hotmail.com>
2017-07-15 23:00:17 -0300

    Update vuejsform.js

--- vueJS/js/vuejsform.js
+++ vueJS/js/vuejsform.js
@@ -35,34 +35,6 @@ new Vue ({
         alert("Usuário cadastrado com sucesso");    
     }
 
-    showData: function(){
-
-        var a = 0;
-    if (localStorage.length == 0) {
-        alert("Não existem cadastrados");
-    }
-    else {
-        while (a+1 < localStorage.length) {
-        
-                  
-
-            email: localStorage.email,
-            $(".collection").prepend("<br> ", "<b>Cadastro Número: </b>", a, "<br><br>");
-            $(".collection").prepend("<b>UF: </b>", show["uf"], "<br>");
-            $(".collection").prepend("<b>Cep: </b>", show["cep"], "<br>");
-            $(".collection").prepend("<b>Munícipio: </b>", show["municipio"], "<br>");
-            $(".collection").prepend("<b>Complemeto: </b>", show["complemento"], "<br>");
-            $(".collection").prepend("<b>Endereço: </b>", show["endereco"], "<br>");
-            $(".collection").prepend("<b>Curso: </b>", show["curso"], "<br>");
-            $(".collection").prepend("<b>Sexo: </b>", show["sexo"], "<br>");
-            $(".collection").prepend("<b>Nome: </b>", show["nome"], "<br>");
-            $(".collection").prepend("<b>Email: </b>", show["email"], "<br>");
-                        
-            a++;
-            
-       
-        }
-    }
     }
       
   }       
